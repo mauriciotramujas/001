ed3b6e0c775b9b76a0c61143d7823009afc8935b
mauriciotramujas <168373091+mauriciotramujas@users.noreply.github.com>
2025-08-29 23:08:10 -0300

    Updated supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = https: "https://rmbaasdfpwmqhisgjoxo.supabase.co"// import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseUrl = https: "https://rmbaasdfpwmqhisgjoxo.supabase.co";// import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtYmFhc2RmcHdtcWhpc2dqb3hvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Mzg5MjIxMiwiZXhwIjoyMDY5NDY4MjEyfQ.Rx9sg_BzN_EATdxWgOhB8Bnm80Wyl5xFHrenfBwSvEM" //import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
